5b4c5c8dd2aa84e18695911405fa52daf438b9ce
Tim Baker <treectrl@users.sf.net>
2012-04-11 22:21:38 -0700

    Don't draw lots when the ObjectGroup layer containing the MapObject is hidden.

--- src/tiled/ZomboidScene.cpp
+++ src/tiled/ZomboidScene.cpp
@@ -22,6 +22,7 @@
 #include "mapobject.h"
 #include "mapobjectitem.h"
 #include "maprenderer.h"
+#include "objectgroup.h"
 #include "tilelayer.h"
 #include "tilelayeritem.h"
 #include "zlot.hpp"
@@ -54,6 +55,8 @@ void ZomboidTileLayerGroup::prepareDrawing(const MapRenderer *renderer, const QR
 {
 	mPreparedLotLayers.clear();
 	foreach (MapObject *mapObject, mMapScene->mLotMapObjects) {
+		if (mapObject->objectGroup()->isVisible() == false)
+			continue;
 		QPoint mapObjectPos = mapObject->position().toPoint();
 		ZLot *lot = mMapScene->mMapObjectToLot[mapObject];
 		const ZTileLayerGroup *layerGroup = lot->tileLayersForLevel(mLevel);
