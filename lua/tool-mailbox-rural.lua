ac1fad7a1d414caaf7d25370f7ddfd96f4ade584
Tim Baker <treectrl@users.sf.net>
2013-09-13 22:41:13 -0700

    Commit correct tiles in tool-mailbox-rural.lua.

--- lua/tool-mailbox-rural.lua
+++ lua/tool-mailbox-rural.lua
@@ -1,10 +1,10 @@
 params = {}
 params.undoText = 'Mailbox (Rural)'
 params.layer0 = '0_Furniture'
-params.tile0 = { w = 'street_decoration_01_3',
-                 n = 'street_decoration_01_2',
-                 e = 'street_decoration_01_1',
-                 s = 'street_decoration_01_0' }
+params.tile0 = { w = 'street_decoration_01_20',
+                 n = 'street_decoration_01_21',
+                 e = 'street_decoration_01_18',
+                 s = 'street_decoration_01_19' }
 
 dofile(scriptDirectory..'/tool-four-directions.lua')
 
