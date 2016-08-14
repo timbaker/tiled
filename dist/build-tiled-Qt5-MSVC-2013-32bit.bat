875d52db2f2b35878126affa1f80b7a6a586aa58
Tim Baker <treectrl@users.sf.net>
2016-08-13 17:06:36 -0700

    Updated to Qt 5.7.

--- dist/build-tiled-Qt5-MSVC-2013-32bit.bat
+++ dist/build-tiled-Qt5-MSVC-2013-32bit.bat
@@ -1,7 +1,7 @@
 call "D:\VisualStudioCommunity2013\VC\vcvarsall.bat" x86
 mkdir C:\Programming\TileZed\dist32
 cd C:\Programming\TileZed\dist32
-"C:\Programming\QtSDK2015\5.5\msvc2013\bin\qmake.exe" C:\Programming\TileZed\tiled\tiled.pro -r -spec win32-msvc2013 "CONFIG+=release"
+"C:\Programming\QtSDK2015\5.7\msvc2013\bin\qmake.exe" C:\Programming\TileZed\tiled\tiled.pro -r -spec win32-msvc2013 "CONFIG+=release"
 "C:\Programming\QtSDK2015\Tools\QtCreator\bin\jom.exe"
 "C:\Programming\TclTk\8.5.x\32bit-mingw\bin\tclsh85.exe" C:\Programming\TileZed\tiled\dist\make-dist.tcl 32bit
 PAUSE
