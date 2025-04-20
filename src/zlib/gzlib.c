b9a118b713e93a52f82e6e6f6ae43ebddc767a03
Tim Baker <timnbaker@gmail.com>
2025-04-20 08:23:40 -0700

    Included unistd.h on Linux in three zlib files.

--- src/zlib/gzlib.c
+++ src/zlib/gzlib.c
@@ -10,6 +10,10 @@
 #include <unistd.h>
 #endif
 
+#if !defined(__APPLE) && !defined(_WIN32)
+#include <unistd.h>
+#endif
+
 #if defined(_WIN32) && !defined(__BORLANDC__)
 #  define LSEEK _lseeki64
 #else
