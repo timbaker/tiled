919dfa66f8d8f829590e7ef45438fb5fd19f4493
Tim Baker <timbaker@theindiestone.com>
2025-10-17 11:34:25 -0700

    Changed parking-stall.lua to create lines 5 tiles long.

--- lua/parking-stall.lua
+++ lua/parking-stall.lua
@@ -9,7 +9,8 @@ function west_parking_stall(x,y)
 	{ LayerLines, x, y, 'street_trafficlines_01_2' },
 	{ LayerLines, x+1, y, 'street_trafficlines_01_2' },
 	{ LayerLines, x+2, y, 'street_trafficlines_01_2' },
-	{ LayerLines, x+3, y, 'street_trafficlines_01_2' }
+	{ LayerLines, x+3, y, 'street_trafficlines_01_2' },
+	{ LayerLines, x+4, y, 'street_trafficlines_01_2' }
     }
 end
 
@@ -18,6 +19,7 @@ function east_parking_stall(x,y)
 	{ LayerBumper, x, y, 'street_decoration_01_45' },
 	{ LayerBumper, x, y+1, 'street_decoration_01_44' },
 	{ LayerBumper, x, y+2, 'street_decoration_01_43' },
+	{ LayerLines, x-4, y, 'street_trafficlines_01_2' },
 	{ LayerLines, x-3, y, 'street_trafficlines_01_2' },
 	{ LayerLines, x-2, y, 'street_trafficlines_01_2' },
 	{ LayerLines, x-1, y, 'street_trafficlines_01_2' },
@@ -33,7 +35,8 @@ function north_parking_stall(x,y)
 	{ LayerLines, x, y, 'street_trafficlines_01_0' },
 	{ LayerLines, x, y+1, 'street_trafficlines_01_0' },
 	{ LayerLines, x, y+2, 'street_trafficlines_01_0' },
-	{ LayerLines, x, y+3, 'street_trafficlines_01_0' }
+	{ LayerLines, x, y+3, 'street_trafficlines_01_0' },
+	{ LayerLines, x, y+4, 'street_trafficlines_01_0' }
     }
 end
 
@@ -42,10 +45,13 @@ function south_parking_stall(x,y)
 	{ LayerBumper, x, y, 'street_decoration_01_40' },
 	{ LayerBumper, x+1, y, 'street_decoration_01_41' },
 	{ LayerBumper, x+2, y, 'street_decoration_01_42' },
+	{ LayerLines, x, y-4, 'street_trafficlines_01_0' },
 	{ LayerLines, x, y-3, 'street_trafficlines_01_0' },
 	{ LayerLines, x, y-2, 'street_trafficlines_01_0' },
 	{ LayerLines, x, y-1, 'street_trafficlines_01_0' },
 	{ LayerLines, x, y, 'street_trafficlines_01_0' }
     }
 end
+
+
 
