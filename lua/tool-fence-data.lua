6a69fc939c3fbc574e4b16316163c0bdf55a337f
Tim Baker <timbaker@theindiestone.com>
2025-11-08 17:20:47 -0800

    Added fences to the fence tool.

--- lua/tool-fence-data.lua
+++ lua/tool-fence-data.lua
@@ -1,4 +1,60 @@
 fence {
+    label = 'Short Black Bars',
+    west1 = 'fencing_01_2',
+    west2 = 'fencing_01_2',
+    gate_space_w = nil,
+    gate_door_w = 'fixtures_doors_fences_01_0',
+    north1 = 'fencing_01_1',
+    north2 = 'fencing_01_1',
+    gate_space_n = nil,
+    gate_door_n = 'fixtures_doors_fences_01_1',
+    nw = 'fencing_01_3',
+    post = 'fencing_01_0'
+}
+
+fence {
+    label = 'Tall Black Bars',
+    west1 = 'fencing_01_67',
+    west2 = 'fencing_01_66',
+    gate_space_w = nil,
+    gate_door_w = nil,
+    north1 = 'fencing_01_64',
+    north2 = 'fencing_01_65',
+    gate_space_n = nil,
+    gate_door_n = nil,
+    nw = 'fencing_01_68',
+    post = 'fencing_01_69'
+}
+
+fence {
+    label = 'Tall Construction',
+    west1 = 'fencing_01_83',
+    west2 = 'fencing_01_82',
+    gate_space_w = nil,
+    gate_door_w = nil,
+    north1 = 'fencing_01_80',
+    north2 = 'fencing_01_81',
+    gate_space_n = nil,
+    gate_door_n = nil,
+    nw = 'fencing_01_84',
+    post = nil
+}
+
+fence {
+    label = 'Tall Construction (Barbed Wire)',
+    west1 = 'fencing_01_91',
+    west2 = 'fencing_01_90',
+    gate_space_w = nil,
+    gate_door_w = nil,
+    north1 = 'fencing_01_88',
+    north2 = 'fencing_01_89',
+    gate_space_n = nil,
+    gate_door_n = nil,
+    nw = 'fencing_01_92',
+    post = nil
+}
+
+fence {
     label = 'Short Wooden',
     west1 = 'fencing_01_35',
     west2 = 'fencing_01_34',
@@ -44,14 +100,26 @@ fence {
     label = 'Short Chainlink',
     west1 = 'fencing_01_27',
     west2 = 'fencing_01_26',
-    gate_space_w = 'fencing_01_30',
+    gate_space_w = nil,
     gate_door_w = 'fixtures_doors_fences_01_16',
     north1 = 'fencing_01_24',
     north2 = 'fencing_01_25',
-    gate_space_n = 'fencing_01_31',
+    gate_space_n = nil,
     gate_door_n = 'fixtures_doors_fences_01_17',
     nw = 'fencing_01_28',
     post = 'fencing_01_29'
+}
+
+fence {
+    label = 'Tall Chainlink',
+    west1 = 'fencing_01_59',
+    west2 = 'fencing_01_58',
+    gate_space_w = 'fencing_01_62',
+    north1 = 'fencing_01_56',
+    north2 = 'fencing_01_57',
+    gate_space_n = 'fencing_01_63',
+    nw = 'fencing_01_60',
+    post = 'fencing_01_61'
 }
 
 fence {
@@ -124,5 +192,17 @@ fence {
     post = 'fencing_01_53'
 }
 
-
+fence {
+    label = 'Short Picket',
+    west1 = 'fencing_01_121',
+    west2 = 'fencing_01_121',
+    gate_space_w = nil,
+    gate_door_w = nil,
+    north1 = 'fencing_01_120',
+    north2 = 'fencing_01_120',
+    gate_space_n = nil,
+    gate_door_n = nil,
+    nw = 'fencing_01_122',
+    post = 'fencing_01_123'
+}
 
