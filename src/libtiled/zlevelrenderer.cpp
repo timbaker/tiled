4501be9c3d50fe2514e0c99eda4b49f4d4f3ed6e
Tim Baker <timbaker@theindiestone.com>
2025-11-27 12:46:06 -0800

    Grid lines are thicker when zoomed in.

--- src/libtiled/zlevelrenderer.cpp
+++ src/libtiled/zlevelrenderer.cpp
@@ -193,6 +193,8 @@ void ZLevelRenderer::drawGrid(QPainter *painter, const QRectF &rect,
 
     QPen pen;
     pen.setCosmetic(true);
+    qreal scaleX = painter->transform().m11();
+    pen.setWidthF(std::max(scaleX, 1.0));
     QBrush brush(gridColor, Qt::Dense4Pattern);
     brush.setTransform(QTransform::fromScale(1/painter->transform().m11(),
                                              1/painter->transform().m22()));
