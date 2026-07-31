93254f4d17ec91335b64198406dc002bd3ae2016
Tim Baker <timbaker@theindiestone.com>
2026-07-31 15:25:34 -0700

    Changes for creating a distribution with linuxdeploy.

--- dist/make-dist-linux64.sh
+++ dist/make-dist-linux64.sh
@@ -1,53 +1,14 @@
-QTVER=5.15.2
-QTDIR_LIB=/opt/qt/$QTVER/gcc_64/lib
-QTDIR_PLUGINS=$(qtpaths --plugin-directory)
 SRC=$(pwd)/..
 BUILD=$SRC/../build-tiled-docker
+APPIMAGE=$SRC/../linuxdeploy/TileZed-x86_64.AppImage
 DESTROOT=$SRC/../dist-tiled-docker
 DEST=$DESTROOT/TileZed
 
 mkdir $DESTROOT
 mkdir $DEST
-cp -avr $BUILD/bin/ $DEST
+cp -a $APPIMAGE $DEST
 cp -ar $BUILD/share/ $DEST
 
-mkdir $DEST/lib
-cp -a $BUILD/lib/libtiled.so.1.0.0 $DEST/lib/libtiled.so.1
-cp -a $BUILD/lib/libzlib1.so.1.0.0 $DEST/lib/libzlib1.so.1
-#for file in $BUILD/lib/libtiled.so*; do cp -a "$file" "$DEST/lib/"; done
-#for file in $BUILD/lib/libzlib1.so*; do cp -a "$file" "$DEST/lib/"; done
-
-cp -a $SRC/dist/qt.conf.linux $DEST/bin/qt.conf
-cp -a $SRC/dist/TileZed.sh $DEST
-chmod +x $DEST/TileZed.sh
-
-cp $QTDIR_LIB/libQt5Core.so.$QTVER $DEST/lib/libQt5Core.so.5
-cp $QTDIR_LIB/libQt5DBus.so.$QTVER $DEST/lib/libQt5DBus.so.5
-cp $QTDIR_LIB/libQt5Gui.so.$QTVER $DEST/lib/libQt5Gui.so.5
-cp $QTDIR_LIB/libQt5Network.so.$QTVER $DEST/lib/libQt5Network.so.5
-cp $QTDIR_LIB/libQt5OpenGL.so.$QTVER $DEST/lib/libQt5OpenGL.so.5
-cp $QTDIR_LIB/libQt5Widgets.so.$QTVER $DEST/lib/libQt5Widgets.so.5
-cp $QTDIR_LIB/libicudata.so.56.1 $DEST/lib/libicudata.so.56
-cp $QTDIR_LIB/libicui18n.so.56.1 $DEST/lib/libicui18n.so.56
-cp $QTDIR_LIB/libicuuc.so.56.1 $DEST/lib/libicuuc.so.56
-cp $QTDIR_LIB/libQt5XcbQpa.so.$QTVER $DEST/lib/libQt5XcbQpa.so.5
-
-#for file in $QTDIR/lib/libQt5Gui.so*; do cp -a "$file" "$DEST/lib/"; done
-#for file in $QTDIR/lib/libQt5Network.so*; do cp -a "$file" "$DEST/lib/"; done
-#for file in $QTDIR/lib/libQt5OpenGL.so*; do cp -a "$file" "$DEST/lib/"; done
-#for file in $QTDIR/lib/libQt5Widgets.so*; do cp -a "$file" "$DEST/lib/"; done
-#for file in $QTDIR/lib/libicu*.so*; do cp -a "$file" "$DEST/lib/"; done
-#for file in $QTDIR/lib/libQt5DBus.so*; do cp -a "$file" "$DEST/lib/"; done
-#for file in $QTDIR/lib/libQt5XcbQpa.so*; do cp -a "$file" "$DEST/lib/"; done
-
-mkdir $DEST/plugins
-mkdir $DEST/plugins/imageformats
-cp -a $QTDIR_PLUGINS/imageformats/libqgif.so $DEST/plugins/imageformats/
-cp -a $QTDIR_PLUGINS/imageformats/libqjpeg.so $DEST/plugins/imageformats/
-mkdir $DEST/plugins/platforms
-cp -a $QTDIR_PLUGINS/platforms/libqxcb.so $DEST/plugins/platforms/
-#cp -a $QTDIR_PLUGINS/platforms/libqwayland-egl.so $DEST/plugins/platforms/
-#cp -a $QTDIR_PLUGINS/platforms/libqwayland-generic.so $DEST/plugins/platforms/
-#cp -a $QTDIR_PLUGINS/platforms/libqwayland-xcomposite-egl.so $DEST/plugins/platforms/
-#cp -a $QTDIR_PLUGINS/platforms/libqwayland-xcomposite-glx.so $DEST/plugins/platforms/
+cp -a $SRC/dist/TileZed-x86_64.AppImage.sh $DEST
+chmod +x $DEST/TileZed-x86_64.AppImage.sh
 
--- docker/build-with-qmake.sh
+++ docker/build-with-qmake.sh
@@ -1,3 +1,7 @@
+#sudo docker run --rm -v "$(pwd)/../..":/workspace qt-builder \
+#    bash -c "ls /opt/qt/6.11.1"
+
 sudo docker run --rm -v "$(pwd)/../..":/workspace qt-builder \
     bash -c "mkdir -p build-tiled-docker && cd build-tiled-docker && qmake ../tiled/tiled.pro INSTALL_ONLY_BUILD=1 && make -j$(nproc) && make install"
 
+sudo chown -R $USER:$USER ../../build-tiled-docker
