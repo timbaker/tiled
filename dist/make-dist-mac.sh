37476bead2f59aa11b43ee39e9fe69c0296eeb52
Tim Baker <timnbaker@gmail.com>
2026-08-02 12:22:45 -0700

    Removed macdeployqt.sh.  Changed make-dist-mac.sh to remove symlinks.

--- dist/make-dist-mac.sh
+++ dist/make-dist-mac.sh
@@ -1,12 +1,23 @@
+#!/bin/bash
 SRC=$(pwd)/..
-BUILD=$SRC/../Qt_6_11_1_for_macOS-Release/bin/
-DESTROOT=$SRC/../../ProjectZomboid
+BUILD=$(realpath $SRC/../Qt_6_11_1_for_macOS-Release/bin)
+DESTROOT=$(realpath $SRC/../../ProjectZomboid)
 DEST=$DESTROOT/TileZed
 
 mkdir -p $DESTROOT
 mkdir -p $DEST
 
+rm -rf $DEST/TileZed.app
 cp -a $BUILD/TileZed.app $DEST
+
+# Add all necessary Qt libraries
+~/Qt/6.11.1/macOS/bin/macdeployqt $DEST/TileZed.app
+
+# Remove all symlinks, they won't work on Steam
+find $DEST/TileZed.app/Contents -type l -delete
+
+mv $DEST/TileZed.app/Contents/Frameworks/libtiled.1.0.0.dylib $DEST/TileZed.app/Contents/Frameworks/libtiled.1.dylib
+mv $DEST/TileZed.app/Contents/Frameworks/libzlib1.1.0.0.dylib $DEST/TileZed.app/Contents/Frameworks/libzlib1.1.dylib
 
 cp -a $SRC/AUTHORS $DEST
 cp -a $SRC/COPYING $DEST
