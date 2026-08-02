268acfac22eba54eb64ad7d3a3b4e2f1e71a6094
Tim Baker <timnbaker@gmail.com>
2026-08-02 10:31:02 -0700

    Fixed make-dist-mac.sh

--- dist/make-dist-mac.sh
+++ dist/make-dist-mac.sh
@@ -3,16 +3,16 @@ BUILD=$SRC/../Qt_6_11_1_for_macOS-Release/bin/
 DESTROOT=$SRC/../../ProjectZomboid
 DEST=$DESTROOT/TileZed
 
-mkdir $DESTROOT
-mkdir $DEST
+mkdir -p $DESTROOT
+mkdir -p $DEST
 
-cp -ra $BUILD/TileZed.app $DEST
+cp -a $BUILD/TileZed.app $DEST
 
-cp -a $SRC/LICENSE.AUTHORS.txt $DEST
-cp -a $SRC/COPYING.txt $DEST
-cp -a $SRC/LICENSE.APACHE.txt $DEST
-cp -a $SRC/LICENSE.BSD.txt $DEST
-cp -a $SRC/LICENSE.GPL.txt $DEST
+cp -a $SRC/AUTHORS $DEST
+cp -a $SRC/COPYING $DEST
+cp -a $SRC/LICENSE.APACHE $DEST
+cp -a $SRC/LICENSE.BSD $DEST
+cp -a $SRC/LICENSE.GPL $DEST
 cp -a $SRC/LICENSE.QT6 $DEST
 
 
