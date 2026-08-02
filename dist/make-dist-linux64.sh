bdd5eab9929302b2d5d61f20b4d31c04d46a230f
Tim Baker <timbaker@theindiestone.com>
2026-08-02 13:43:23 -0700

    Fixed make-dist-linux64.sh

--- dist/make-dist-linux64.sh
+++ dist/make-dist-linux64.sh
@@ -4,18 +4,18 @@ APPIMAGE=$SRC/../linuxdeploy-tiled/TileZed-x86_64.AppImage
 DESTROOT=$SRC/../dist-tiled-docker
 DEST=$DESTROOT/TileZed
 
-mkdir $DESTROOT
-mkdir $DEST
+mkdir -p $DESTROOT
+mkdir -p $DEST
 cp -a $APPIMAGE $DEST
 cp -ar $BUILD/share/ $DEST
 
 cp -a $SRC/dist/TileZed-x86_64.AppImage.sh $DEST
 chmod +x $DEST/TileZed-x86_64.AppImage.sh
 
-cp -a $SRC/LICENSE.AUTHORS.txt $DEST
-cp -a $SRC/COPYING.txt $DEST
-cp -a $SRC/LICENSE.APACHE.txt $DEST
-cp -a $SRC/LICENSE.BSD.txt $DEST
-cp -a $SRC/LICENSE.GPL.txt $DEST
+cp -a $SRC/LICENSE.AUTHORS $DEST
+cp -a $SRC/COPYING $DEST
+cp -a $SRC/LICENSE.APACHE $DEST
+cp -a $SRC/LICENSE.BSD $DEST
+cp -a $SRC/LICENSE.GPL $DEST
 cp -a $SRC/LICENSE.QT6 $DEST
 
